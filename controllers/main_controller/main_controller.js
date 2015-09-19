f6fd40f65dfe9ef7f35a77abfc6a56ef2db97e1f
Omer Tashir <omer_tashir@hotmail.com>
2015-09-19 19:24:14 +0300

    asd

--- controllers/main_controller/main_controller.js
+++ controllers/main_controller/main_controller.js
@@ -2,17 +2,51 @@
 (function(){
     angular.module('main_module')
         .controller('main_controller', function($scope, $http){
+
+            $scope.sort_order = 'full';
+            $scope.choice = null;
+            $scope.checked_status = 'false';
+
+            $scope.changeCheck = function(){
+                if($scope.checked_status == 'false'){
+                    $scope.checked_status = '';
+                }
+                else{
+                    $scope.checked_status = 'false';
+                }
+            };
+
             $scope.getProfiles = function(){
                 $http.get('http://duda-api-test.herokuapp.com/profiles')
                     .then(function(result){
                         $scope.profiles = result.data;
                     });
             };
-            $scope.getFullProfileData = function(profile_id){
-                $http.get('http://duda-api-test.herokuapp.com/profile/'+profile_id)
-                    .then(function(result){
-                        $scope.full_profile = result.data;
-                    });
-            }
+
+            $scope.fullProfiles = [];
+            $scope.getFullProfileData = function(){
+                while ($scope.profiles.length > 0) {
+                    var singleProfile = $scope.profiles.pop();
+                    $http.get('http://duda-api-test.herokuapp.com/profile/'+singleProfile.id)
+                        .then(function(result){
+                            $scope.fullProfiles.push(result.data);
+                        });
+                }
+            };
+
+            $scope.refresh = function () {
+                $scope.profiles = "";
+                $scope.fullProfiles = [];
+                $scope.getProfiles();
+            };
+
+            $scope.populate = function(profile){
+                $scope.name_search_input = profile.full;
+                $scope.bio_search_input = profile.bio;
+            };
+
+            $scope.create_card = function(full, bio){
+                fullProfiles.push({"id":"","full":full,"bio":bio,"fbprof":"","isprivate":false});
+            };
         });
 })();
